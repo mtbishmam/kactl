af6de847dcdb6e32a6f40e26aa1f067ed2df5d40
mtbishmam <mtbishmam@gmail.com>
2026-06-24 16:21:24 +0600

    Modify and include code for ModularArithmetic.h

--- content/number-theory/ModularArithmetic.h
+++ content/number-theory/ModularArithmetic.h
@@ -1,30 +1,27 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-09-28
- * License: CC0
- * Source: folklore
- * Description: Operators for modular arithmetic. You need to set {\tt mod} to
+ * Author: MTB Ishmam
+ * Date: 2026-06-24
+ * Description: Operators for modular arithmetic. You need to set {\tt MOD} to
  * some number first and then you can use the structure.
  */
 #pragma once
-
 #include "euclid.h"
-
-const ll mod = 17; // change to something else
-struct Mod {
-	ll x;
-	Mod(ll xx) : x(xx) {}
-	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
-	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
-	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
-	Mod operator/(Mod b) { return *this * invert(b); }
-	Mod invert(Mod a) {
-		ll x, y, g = euclid(a.x, mod, x, y);
-		assert(g == 1); return Mod((x + mod) % mod);
-	}
-	Mod operator^(ll e) {
-		if (!e) return Mod(1);
-		Mod r = *this ^ (e / 2); r = r * r;
-		return e&1 ? *this * r : r;
-	}
+const ll MOD = 1e9 + 7; // change to something else
+struct mint {
+    ll x;
+    mint(ll _ = 0) : x((_ % MOD + MOD) % MOD) {}
+    mint operator+(const mint& b) { return mint(x + b.x >= MOD ? x + b.x - MOD : x + b.x); }
+    mint operator-(const mint& b) { return mint(x - b.x < 0 ? x - b.x + MOD : x - b.x); }
+    mint operator*(const mint& b) { return mint((x * b.x) % MOD); }
+    // mint operator*(const mint& b) { return mint(modmul(x, b.x, MOD)); }
+    mint operator/(const mint& b) { return *this * invert(b); }
+    static mint invert(const mint& a) {
+        ll x, y, g = euclid(a.x, MOD, x, y);
+        assert(g == 1); return mint((x + MOD) % MOD);
+    }
+    mint operator^(ll e) {  // a^b^c = (a^b)^c, not a^(b^c) [natural ord]
+        if (!e) return mint(1);
+        mint r = *this ^ (e / 2); r = r * r;
+        return e & 1 ? *this * r : r;
+    }
 };
