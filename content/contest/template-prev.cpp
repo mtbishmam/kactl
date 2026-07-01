cb7bd03d1b2cbdf55a6a2ac07da83f942715472b
mtbishmam <mtbishmam@gmail.com>
2026-07-01 23:24:24 +0600

    Update template.cpp and remove OrderStatisticTree.h

---
+++ content/contest/template-prev.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifndef DeBuG
+	#define dbg(...)
+#endif
+
+#define int int64_t
+
+#define endl '\n'
+#define sz(x) (int) (x).size()
+#define all(x) begin(x), end(x)
+template<class T> using V = vector<T>;
+using ll = long long; using pii=pair<int,int>;
+using vi = V<int>; using vvi = V<vi>;
+using vb = V<bool>;
+
+void solve(int cs) {}
+signed main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int tc = 1;
+// #ifdef DeBuG
+	cin >> tc;
+// #endif
+	for (int cs = 1; cs <= tc; cs++) solve(cs);
+}
--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,33 +1,35 @@
-/**
- * Author: MTB Ishmam
- * Date: 2026-06-23
- * Description: 
- */
-// BRACU_DireCrows
-#include "bits/stdc++.h" /** keep-include */
+#include "bits/stdc++.h"
 using namespace std;
-
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 #ifndef DeBuG
-	#define dbg(...)
+#define dbg(...)
+#define TT template <typename T
 #endif
+#define TU TT, typename U>
 
 #define int int64_t
-
 #define endl '\n'
 #define sz(x) (int) (x).size()
 #define all(x) begin(x), end(x)
-template<class T> using V = vector<T>;
+#define rep(i,a,b) for(int i=a; i<(b); ++i)
+TU bool chmin(T&a,U b){return a>b?(a=b,1):0;}
+TU bool chmax(T&a,U b){return a<b?(a=b,1):0;}
+TT> using V = vector<T>; using vi = V<int>;
 using ll = long long; using pii=pair<int,int>;
-using vi = V<int>; using vvi = V<vi>;
-using vb = V<bool>;
+using vb = V<bool>; using vvi = V<vi>;
+// TT> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rand(l, r) uniform_int_distribution<ll>(l, r)(rng)
 
-void solve(int cs) {}
+void solve(int cs) {
+}
 signed main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	int tc = 1;
-// #ifdef DeBuG
-	cin >> tc;
-// #endif
-	for (int cs = 1; cs <= tc; cs++) solve(cs);
+    cin.tie(0)->sync_with_stdio(0);
+    int tc = 1;
+    // #ifdef DeBuG
+    cin >> tc;
+    // #endif
+    for (int cs = 1; cs <= tc; cs++) solve(cs);
 }
